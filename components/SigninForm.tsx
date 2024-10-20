4dc6a8eb9fc978e72abe7ef04650fe0261b8c34f
Stanislav Aituhanov <s.aituhanov@mmdsmart.com>
2024-10-20 13:11:52 +0300

    Signup & Signin

--- components/SigninForm.tsx
+++ components/SigninForm.tsx
@@ -1,10 +1,17 @@
 'use client'
+import { signinUser } from '@/actions/auth'
 import { Input } from '@nextui-org/react'
 import Link from 'next/link'
+import { useFormState } from 'react-dom'
+import SubmitButton from './SubmitButton'
 
 const SigninForm = () => {
+  const [formState, action] = useFormState(signinUser, { message: null })
   return (
-    <form className="bg-content1 border border-default-100 shadow-lg rounded-md p-3 flex flex-col gap-2 ">
+    <form
+      action={action}
+      className="bg-content1 border border-default-100 shadow-lg rounded-md p-3 flex flex-col gap-2 "
+    >
       <h3 className="my-4">Sign in</h3>
       <Input
         fullWidth
@@ -22,6 +29,8 @@ const SigninForm = () => {
         type="password"
         placeholder="Password"
       />
+
+      <SubmitButton label="Sign in" />
 
       <div>
         <Link href="/signup">{`Don't have an account?`}</Link>
